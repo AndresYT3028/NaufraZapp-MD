7b32f28d31480518a08ad644328785b5f00081cc
Support-Bots-OFC <155838423+Bots-WhatsApp-OFC@users.noreply.github.com>
2024-03-15 11:29:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 /* ❒═════════════════◊【】◊═════════════════❒ */
-say('NaufraZapp\nMD', {
+say('Naufra\nZapp\nMD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});

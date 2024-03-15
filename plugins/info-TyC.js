acc457673f6c828f777aef0d4c4538caef20e332
Support-Bots-OFC <155838423+Bots-WhatsApp-OFC@users.noreply.github.com>
2024-03-15 11:20:08 -0300

    Delete plugins directory

--- plugins/info-TyC.js
+++
@@ -1,9 +0,0 @@
-const handler = async (m, {conn}) => {
-  m.reply(global.terminos);
-};
-handler.customPrefix = /términos y condiciones y privacidad|terminosycondicionesyprivacidad|terminosycondiciones|terminos y condiciones y privacidad|terminos y condiciones|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones/i;
-handler.command = new RegExp;
-export default handler;
-
-global.terminos = `EN LOS TERMINOS Y CONDICIONES
-TODAVIA NO EATA DISPONIBLE.`;
--- plugins/owner-banchat.js
+++
@@ -1,9 +0,0 @@
-const handler = async (m) => {
-  global.db.data.chats[m.chat].isBanned = true;
-  m.reply('✅️ *Chat Baneado Con Exito, Ya No Podran Usar SakuraBotLite - MD*');
-};
-handler.help = ['banchat'];
-handler.tags = ['owner'];
-handler.command = /^banchat$/i;
-handler.rowner = true;
-export default handler;
